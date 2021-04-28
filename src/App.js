14f19de0046fb00b260b254dc8fe7d35412b08dd
jagodarybacka <jagoda.roza.rybacka@gmail.com>
2021-04-28 21:55:54 +0200

    Basic game state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { useState } from 'react';
 import Gandalf from './characters/Gandalf';
 import Sauron from './characters/Sauron';
+import useGame from './state';
 import './App.scss';
 
 function App() {
-  const [ gandalf, setGandalf ] = useState({ health: 100 });
-  const [ sauron, setSauron ] = useState({ health: 99 });
+  const [ state, dispatch ] = useGame();
 
   return (
     <div className="App">
-      <Gandalf {...gandalf} />
-      <Sauron {...sauron} />
+      <Gandalf {...state.gandalf} />
+      <Sauron {...state.sauron} />
     </div>
   );
 }
---
+++ src/state/consts.js
@@ -0,0 +1,8 @@
+export const DEFAULT_STATE = {
+  gandalf: {
+    health: 100
+  },
+  sauron: {
+    health: 100
+  }
+};
